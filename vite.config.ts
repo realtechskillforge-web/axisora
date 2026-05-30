22bbc14bb62b9814934d5bb9eac16a8adf60317f
Vamsi H <vamsih@Vamsis-MacBook-Air.local>
2026-05-30 21:52:30 +0530

    fixed basepath issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/axisora/",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
