aa3697b29095fc04cfe4d4dd863c401c1275585d
Vamsi H <vamsih@Vamsis-MacBook-Air.local>
2026-05-30 08:39:42 +0530

    build failure issue fixed after vite-plugin-renderer added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
   base: "/",
@@ -12,30 +11,6 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
-    prerender({
-      staticDir: "dist",
-      routes: [
-        "/",
-        "/about",
-        "/support",
-        "/programs",
-        "/programs/java-fullstack-angular",
-        "/programs/ai-java-backend",
-        "/programs/angular-frontend",
-        "/programs/flutter",
-        "/programs/android-java",
-        "/programs/digital-marketing",
-        "/programs/java-placement",
-        "/programs/python-placement",
-        "/freelancing",
-        "/freelancing/full-stack",
-        "/freelancing/frontend",
-        "/freelancing/wordpress-shopify",
-        "/freelancing/static-sites",
-        "/freelancing/digital-marketing",
-        "/freelancing/seo",
-      ],
-    }),
   ],
   build: {
     outDir: "dist",
